e0d567074ca4acf0d3c23ff28adb72a2f3eb35fa
Daniel Tomas <dani7sly12345@gmail.com>
2022-11-24 19:43:02 +0000

    Fix action ARI args position

--- src/std_actions/arrays_actions.rs
+++ src/std_actions/arrays_actions.rs
@@ -1,23 +1,23 @@
 use crate::{Procedure, Value, TYPE_NUMB, TYPE_VAR, TYPE_VAR_ARRAY, TYPE_VAR_NUMB};
 
 pub(super) const ACTION_ARI: &str = "ari";
-pub(super) const ACTION_ARI_ARGS: &[&str] = &[TYPE_VAR_ARRAY, TYPE_NUMB, TYPE_VAR];
+pub(super) const ACTION_ARI_ARGS: &[&str] = &[TYPE_VAR, TYPE_VAR_ARRAY, TYPE_NUMB];
 pub(super) fn ari(current_proc: &mut Procedure) {
     let parameters: Vec<String> = current_proc.get_raw_parameters();
 
     assert!(parameters.len() == ACTION_ARI_ARGS.len());
 
-    let param1 = current_proc.get_value(&parameters[0]);
     let param2 = current_proc.get_value(&parameters[1]);
-    let param3 = current_proc.get_variable_value_mutref(&parameters[2]);
+    let param3 = current_proc.get_value(&parameters[2]);
+    let param1 = current_proc.get_variable_value_mutref(&parameters[0]);
 
-    assert!(param1.is_array());
-    assert!(param2.is_numb());
-    assert!(param3.typee() == param1.typee());
+    assert!(param2.is_array());
+    assert!(param3.is_numb());
+    assert!(param1.typee() == param2.typee());
 
-    let i = param2.get_numb_value() as usize;
+    let i = param1.get_numb_value() as usize;
 
-    *param3 = match param1 {
+    *param1 = match param2 {
         Value::Array(_, vec) => vec[i].clone(),
         _ => unreachable!(),
     }
